ea2d83d22a07d15fda670b24b58dab514a99964c
daniel.lorenzi <dlorenzi@compass-group.fr>
2025-03-23 02:39:30 +0100

    mise à jour, corrections

---
+++ src/main/java/com/invoicegenerator/views/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contient les vues de l'interface utilisateur pour l'application de génération de factures.
+ * Ce package inclut les composants JavaFX tels que les écrans de saisie des commandes,
+ * les tableaux de visualisation des factures, et les interfaces de gestion des navettes de facturation.
+ */
+package com.invoicegenerator.views;
